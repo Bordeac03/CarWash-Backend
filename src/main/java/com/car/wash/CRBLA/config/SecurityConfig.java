98680a9e7dfca644c3cdced186c21d5320653ed3
Bordeac03 <bordea37@gmail.com>
2024-04-14 15:54:55 +0300

    cors

--- src/main/java/com/car/wash/CRBLA/config/SecurityConfig.java
+++ src/main/java/com/car/wash/CRBLA/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
+            .cors().disable()
             .authorizeHttpRequests((authz) -> authz
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
