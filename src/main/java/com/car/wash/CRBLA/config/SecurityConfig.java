2215b6ece04ee0edce1e2ef08383029313089ce9
Laurentiu Pomana <laurentiu.nicolae1@gmail.com>
2024-05-20 09:16:28 +0300

    update

--- src/main/java/com/car/wash/CRBLA/config/SecurityConfig.java
+++ src/main/java/com/car/wash/CRBLA/config/SecurityConfig.java
@@ -40,7 +40,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Replace '*' with the specific origin
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")); // Replace
+                                                                                                           // '*' with
+                                                                                                           // the
+                                                                                                           // specific
+                                                                                                           // origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type"));
         configuration.setAllowCredentials(true); // Add this line
